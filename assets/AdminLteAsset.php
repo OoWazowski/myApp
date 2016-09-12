826b86e24cf3c170879be1f660f461f64f5cc80e
OoWazowski <arnatchakun.hack@gmail.com>
2016-09-12 10:36:35 +0700

    Hi Wazowski
    Upto

--- assets/AdminLteAsset.php
+++ assets/AdminLteAsset.php
@@ -1,58 +1,58 @@
 <?php
-/**
- * @link http://www.yiiframework.com/
- * @copyright Copyright (c) 2008 Yii Software LLC
- * @license http://www.yiiframework.com/license/
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
 
-namespace app\assets;
+namespace app\themes\adminlte;
 
 use yii\web\AssetBundle;
 
-/**
- * @author Qiang Xue <qiang.xue@gmail.com>
- * @since 2.0
- */
-class AdminLteAsset extends AssetBundle
-{
+class AdminLteAsset extends AssetBundle {
+
+    public $sourcePath = '@vendor/almasaeed2010/adminlte';
     public $basePath = '@webroot';
     public $baseUrl = '@web';
-    public $bashTo = '@vendor';
+    public $toPath = '@vendor/almasaeed2010/adminlte';
+    public $baseUrl = '@vendor/almasaeed2010/adminlte';
     public $css = [
-        'themes/AdminLTE/bootstrap/css/bootstrap.min.css',
-        'themes/AdminLTE/dist/css/AdminLTE.css',
-        'themes/AdminLTE/dist/css/skins/_all-skins.min.css',
-        'themes/AdminLTE/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css',
-        'themes/AdminLTE/plugins/daterangepicker/daterangepicker.css',
-        'themes/AdminLTE/plugins/datepicker/datepicker3.css',
-        'themes/AdminLTE/plugins/jvectormap/jquery-jvectormap-1.2.2.css',
-        'themes/AdminLTE/plugins/morris/morris.css',
-        'themes/AdminLTE/plugins/iCheck/flat/blue.css',
+        'bootstrap/css/bootstrap.min.css',
+        'dist/css/AdminLTE.css',
+        'dist/css/skins/_all-skins.min.css',
+        'plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css',
+        'plugins/daterangepicker/daterangepicker.css',
+        'plugins/datepicker/datepicker3.css',
+        'plugins/jvectormap/jquery-jvectormap-1.2.2.css',
+        'plugins/morris/morris.css',
+        'plugins/iCheck/flat/blue.css',
         'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css',
         'https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css',
     ];
     public $js = [
-        'themes/AdminLTE/plugins/jQuery/jquery-2.2.3.min.js',
-        'themes/AdminLTE/dist/js/app.min.js',
-        'themes/AdminLTE/dist/js/app.js',
-        'themes/AdminLTE/dist/js/demo.js',
-//        'themes/AdminLTE/dist/js/pages/dashboard.js',
-        'themes/AdminLTE/plugins/slimScroll/jquery.slimscroll.min.js',
-        'themes/AdminLTE/plugins/fastclick/fastclick.js',
-        'themes/AdminLTE/plugins/morris/morris.min.js',
-        'themes/AdminLTE/plugins/sparkline/jquery.sparkline.min.js',
-        'themes/AdminLTE/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js',
-        'themes/AdminLTE/plugins/jvectormap/jquery-jvectormap-world-mill-en.js',
-        'themes/AdminLTE/plugins/knob/jquery.knob.js',
-//        'themes/AdminLTE/plugins/daterangepicker/daterangepicker.js',
-        'themes/AdminLTE/plugins/datepicker/bootstrap-datepicker.js',
-        'themes/AdminLTE/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js',
+        'plugins/jQuery/jquery-2.2.3.min.js',
+        'dist/js/app.min.js',
+        'dist/js/app.js',
+        'dist/js/demo.js',
+//        '/dist/js/pages/dashboard.js',
+        'plugins/slimScroll/jquery.slimscroll.min.js',
+        'plugins/fastclick/fastclick.js',
+        'plugins/morris/morris.min.js',
+        'plugins/sparkline/jquery.sparkline.min.js',
+        'plugins/jvectormap/jquery-jvectormap-1.2.2.min.js',
+        'plugins/jvectormap/jquery-jvectormap-world-mill-en.js',
+        'plugins/knob/jquery.knob.js',
+//        '/plugins/daterangepicker/daterangepicker.js',
+        'plugins/datepicker/bootstrap-datepicker.js',
+        'plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js',
         'https://code.jquery.com/ui/1.11.4/jquery-ui.min.js',
-//        'https://cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js',
-//        'https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.11.2/moment.min.js',
+        'https://cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js',
+        'https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.11.2/moment.min.js',
     ];
     public $depends = [
         'yii\web\YiiAsset',
         'yii\bootstrap\BootstrapAsset',
     ];
+
 }
