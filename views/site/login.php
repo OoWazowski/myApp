e8ee1ae77536485868edbcc076cecb129dd6148c
OoWazowski <arnatchakun.hack@gmail.com>
2016-09-12 17:23:39 +0700

    i@m OK
    i@m OK

--- views/site/login.php
+++ views/site/login.php
@@ -22,11 +22,11 @@ $fieldOptions2 = [
 
 <div class="login-box">
     <div class="login-logo">
-        <a href="#"><b>i</b>New CRM</a>
+        <a href="#"><b>i</b>@m App</a>
     </div>
     <!-- /.login-logo -->
     <div class="login-box-body">
-        <p class="login-box-msg">Sign in to start your session</p>
+        <p class="login-box-msg">Sign in</p>
 
         <?php $form = ActiveForm::begin(['id' => 'login-form', 'enableClientValidation' => false]); ?>
 
